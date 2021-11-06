7e563c9b46a43b2ae96ff2b6ba889a81acabd51a
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-05 17:42:36 -0700

    P2D - done reading. Up next -> exercises for p2d

--- p2/src/App.js
+++ p2/src/App.js
@@ -1,42 +1,78 @@
 import React, { useState, useEffect } from 'react'
-//import Note from './components/Note'
-import axios from 'axios'
+import Note from './components/Note'
+import noteServices from './services/notes'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
   
+  const notesToShow = showAll ? notes : notes.filter( note => note.important === true)
+  
   const hook = () => {
-    console.log('Effect')
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
-        console.log('promise fullfilled')
-        setNotes(response.data)
+    noteServices
+      .getAll()
+      .then(initialNotes => {
+        setNotes(initialNotes)
       })
   }
   useEffect(hook, [])
-  console.log('render', notes.length, 'notes')
 
-  const notesToShow = showAll ? notes : notes.filter( note => note.important === true)
   
   const addNote = (event) => {
+    /*When we create a new Note Obj we let the backend server add the id*/
     event.preventDefault()
     const noteObject = {
       content: newNote,
       date: new Date().toISOString(),
       important: Math.random() < 0.5,
-      id: notes.length + 1,
     }
-    setNotes(notes.concat(noteObject))
-    setNewNote(' ')
+    noteServices
+      .create(noteObject)
+      .then(returnedNote => {
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
+    })
+    /* axios
+      .post('http://localhost:3001/noteshttp://localhost:3001/notes', noteObject)
+      .then(response => {
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      }) */
   }
   
   const handleNoteChange = (event) => {
     console.log(event.target.value)
     setNewNote(event.target.value)
-}
+  }
+
+  const toggleImportanceOfFunc = (id) => {
+    const handler = () => {
+    /* const url = `http://localhost:3001/notes/${id}` */
+    const note = notes.find(n => n.id === id)
+    const changedNote = {...note, important : !note.important}
+    noteServices
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(note => note.id !== id ? note : returnedNote))
+      })
+      .catch(error => {
+        alert(`the note '${note.content}' was already deleted from the server`)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+      /* .then(response => {
+        setNotes(notes.map(note => 
+          note.id !== id ? note : response.data))
+      }) */
+    /* axios
+      .put(url, changedNote)
+      .then(response => {
+        setNotes(notes.map(note => note.id !== id ? note : response.data))
+      }) */
+    }
+    return handler
+  }
+
     return (
     <div>
       <h1>Notes</h1>
@@ -47,8 +83,8 @@ const App = () => {
       </div>
       <ul>
        {notesToShow.map(note => 
-       <Note key={note.id} note={note} />
-       )}
+       <Note key={note.id} note={note} toggleImportance={toggleImportanceOfFunc(note.id)} />
+       )}{/*  or use () => toggleImportanceOf(note.id)  */}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange}/>
---
+++ p2/src/services/notes.js
@@ -0,0 +1,40 @@
+import axios from 'axios'
+const baseUrl = 'http://localhost:3001/notes'
+
+const getAll = () => {
+    /*Returns the notes*/
+    /* return axios.get(baseUrl) */
+    const request = axios.get(baseUrl)
+    const nonExisting = {
+        id : 1000,
+        content: 'Blah blah blah',
+        data: '2019-05-30T17:30:31.098Z',
+        important: true
+    }
+    return request.then(response => response.data.concat(nonExisting))
+}
+
+const create = (newObject) => {
+    const request = axios.post(baseUrl, newObject)
+    return request.then(response => response.data)
+}
+
+const update = (id, newObject) => {
+    const request = axios.put(`${baseUrl}/${id}`, newObject)
+    return request.then(response => response.data)
+}
+
+
+const exportedObjects = {
+    getAll,
+    create,
+    update
+}
+export default exportedObjects
+
+/*This doesn't work. FSO has it wrong. Leaving it here for future reference
+export default {
+    getAll : getAll,
+    create : create,
+    update : update
+}*/
