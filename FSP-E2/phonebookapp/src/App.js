cb8653d75ba5eb12ea9f8206138ead37221da2e7
RetiredDairyCow <atg.ashesh@gmail.com>
2021-11-07 14:37:12 -0800

    moved part 2 - courseinfo files around

--- FSP-E2/phonebookapp/src/App.js
+++ FSP-E2/phonebookapp/src/App.js
@@ -9,7 +9,7 @@ import './index.css'
 
 
 const App = () => {
- /*  const url = 'http://localhost:3001/persons' */
+ 
   const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('Enter Name')
   const [newNumber, setNewNumber] = useState('Enter New Number')
@@ -24,11 +24,6 @@ const App = () => {
       .then(initialList =>{
         setPersons(initialList)
       })
-    /* axios
-      .get('http://localhost:3001/persons')
-      .then(response => {
-        setPersons(response.data)
-      }) */
   }
   useEffect(hook,[])
 
@@ -40,17 +35,22 @@ const App = () => {
     <div>
       
       <h2>Phonebook</h2>
+
       <Notification message={addedMessage}/>
-      {<Search searchTerm={searchTerm} setSearch={setSearch}/>}
+      
+      <Search searchTerm={searchTerm} setSearch={setSearch}/>
      
       <h2>Add New Entry Please</h2>
-      {<Forms newName={newName} newNumber={newNumber} 
+
+      <Forms 
+      newName={newName} newNumber={newNumber} 
       setNewName={setNewName} setNewNumber={setNewNumber}
       persons={persons} setPersons={setPersons}
       addedMessage={addedMessage} setAddedMessage={setAddedMessage}
-      entryID={persons.id} />}
+      entryID={persons.id}/>
 
       <h2>Numbers</h2>
+
       {nameFilter.map(person =>
         <Numbers key={person.id} firstAndLast = {person.name} entryID={person.id}
         no={person.number} persons={persons} setPersons={setPersons} />)}
